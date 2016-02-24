5fdb426a5314a13dba67f434b4b1e43cee42aab8
Eddie Park <eddie.park@id.me>
2016-02-23 19:49:45 -0500

    Create controllers and created about view

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+var homepageController = require('../controllers/homepage_controller');
+var otherController = require('../controllers/other_controller');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+router.get('/', homepageController.index);
+router.get('/about', otherController.about);
 module.exports = router;
